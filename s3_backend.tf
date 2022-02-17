b4f3d481820e7d2b6af6432c61c26b95d0d2298b
xaxt176 <aditya.tewari@grainger.com>
2022-02-16 21:14:18 -0600

    TF

--- s3_backend.tf
+++ s3_backend.tf
@@ -19,3 +19,11 @@ resource "aws_s3_bucket" "terraform_state" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.terraform_state.bucket
 }
+
+terraform {
+  backend "s3" {
+    bucket = aws_s3_bucket.terraform_state.bucket
+    key    = "tfstate"
+    region = "us-east-1"
+  }
+}
